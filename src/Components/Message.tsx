22bebab87b47f33270e836e6496553ee670fbc40
akbarob <akbarbadmus07@gmail.com>
2022-10-04 13:22:19 +0100

    time

--- src/Components/Message.tsx
+++ src/Components/Message.tsx
@@ -3,9 +3,10 @@ import { auth } from "../firebase";
 
 const style = {
   message: `flex items-center shadow-2xl m-4 p-2 rounded-tl-full rounded-tr-full`,
-  name: ` mt-[-4rem] text-gray-600 text-xs `,
-  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
-  received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
+  nameSent: `text-right text-gray-600 text-xs`,
+  nameReceived: `text-left text-gray-600 text-xs`,
+  sent: `bg-[#395dff] text-white text-right float-right rounded-bl-full `,
+  received: `bg-green-100 text-black  float-left  text-left rounded-br-full`,
 };
 
 interface Props {
@@ -18,12 +19,37 @@ export const Message = ({ message, scroll }: Props) => {
     message.uid === auth.currentUser?.uid
       ? `${style.sent}`
       : `${style.received}`;
+
+  const nameClass =
+    message.uid === auth.currentUser?.uid
+      ? `${style.nameSent}`
+      : `${style.nameReceived}`;
   return (
     <div>
+      <p className={nameClass}>
+        {message.name}{" "}
+        <span className="">
+          {new Intl.DateTimeFormat("en-GB", {
+            timeStyle: "short",
+          }).format(message.time)}
+        </span>
+      </p>{" "}
       <div className={`${style.message} ${messageClass}`}>
-        <p className={style.name}>{message.name}</p>
         <p ref={scroll}>{message.text}</p>
       </div>
     </div>
   );
 };
+
+// const Time = () => {
+//   return (
+//     <span>
+//       {new Intl.DateTimeFormat("en-US", {
+//         // year: "numeric",
+//         // month: "long",
+//         // day: "2-digit",
+//         timeStyle: "short",
+//       }).format(message.time)}
+//     </span>
+//   );
+// };
